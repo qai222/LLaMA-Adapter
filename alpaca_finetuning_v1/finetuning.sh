6cf40408e034b6c657ba717fd187d22243d0a06f
qai <qai@mit.edu>
2023-07-04 15:28:55 -0400

    - new finetune for 20230703 (molgpu06) - new dataset

--- alpaca_finetuning_v1/finetuning.sh
+++ alpaca_finetuning_v1/finetuning.sh
@@ -1,15 +1,20 @@
-TARGET_FOLDER=/home/researcher/qai/LLaMA-Adapter/weights/7B
+WORK_FOLDER=/home/qai/LLaMA-Adapter/alpaca_finetuning_v1/finetune_20230703
+DATA_JSON="OrdAlpaca_MaxToken900_TrainSize10000_TestSize2000_inputs-conditions-outcomes-workups.json"
+TARGET_FOLDER=/home/qai/LLaMA-Adapter/weights/7B
 
-torchrun --nproc_per_node 1 finetuning.py \
+mkdir -p $WORK_FOLDER
+
+torchrun --nproc_per_node 2 finetuning.py \
     --model Llama7B_adapter \
     --llama_model_path $TARGET_FOLDER/ \
-    --data_path data/ins_mt900_train.json \
+    --data_path $WORK_FOLDER/$DATA_JSON \
     --adapter_layer 30 \
     --adapter_len 10 \
     --max_seq_len 900 \
     --batch_size 1 \
-    --epochs 50 \
+    --epochs 200 \
     --warmup_epochs 2 \
     --blr 9e-3 \
     --weight_decay 0.02 \
-    --output_dir ./checkpoint/
+    --output_dir $WORK_FOLDER/checkpoint/ \
+    --log_dir $WORK_FOLDER/checkpoint/
