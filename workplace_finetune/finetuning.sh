779dc0416d08741b6f4d4441795aaefaf723094c
qai <qai@mit.edu>
2023-07-31 17:23:21 -0400

    tuning submit 0731

--- workplace_finetune/finetuning.sh
+++ workplace_finetune/finetuning.sh
@@ -1,5 +1,7 @@
 #!/bin/sh
-WORK_FOLDER="fintune_20230716"
+# tmux new -d -s 'llama' 'bash finetuning.sh > finetuning.log'
+export PYTHONPATH="$PWD/../:$PYTHONPATH"
+WORK_FOLDER=$PWD/finetune_20230731
 DATA_FOLDER=$PWD/../data
 #DATA_JSON_NAME="OrdAlpaca_MaxToken900_TrainSize10000_TestSize2000_inputs-conditions-outcomes-workups.json"
 DATA_JSON_NAME="OrdAlpaca_MaxToken900_TrainSize85739_TestSize21435_inputs-conditions-outcomes-workups.json.gz"
@@ -11,12 +13,12 @@ mkdir -p $WORK_FOLDER
 torchrun --nproc_per_node 2 finetuning.py \
     --model Llama7B_adapter \
     --llama_model_path $TARGET_FOLDER/ \
-    --data_path $WORK_FOLDER/$DATA_JSON_PATH \
+    --data_path $DATA_JSON_PATH \
     --adapter_layer 30 \
     --adapter_len 10 \
     --max_seq_len 900 \
     --batch_size 1 \
-    --epochs 100 \
+    --epochs 15 \
     --warmup_epochs 2 \
     --blr 9e-3 \
     --weight_decay 0.02 \
